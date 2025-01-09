87e103820252eb7548524cbc2e5ab96a7f1aa1a0
FranciscoLattaro <lpdcmelo@gmail.com>
2025-01-09 14:29:58 -0300

    traer un track random en el media player

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,7 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { TrackService } from '@modules/tracks/services/track.service';
 import { MultimediaService } from '@shared/services/multimedia.service';
 import { Subscription } from 'rxjs';
+import { environment } from 'src/environtments/environment';
 
 @Component({
   selector: 'app-media-player',
@@ -9,6 +12,7 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
+  private URL = environment.api;
   mockCover: TrackModel = {
     cover: 'https://www.electricdust.net/wp-content/uploads/2021/07/3052fa49-7a5a-1f3e-57b6-cf778c50300a.jpg',
     album: 'Gioli & Assia',
@@ -21,7 +25,7 @@ export class MediaPlayerComponent implements OnInit, OnDestroy {
 
   ]
 
-  constructor(private multimediaService: MultimediaService) {
+  constructor(private multimediaService: MultimediaService, private trackService: TrackService) {
 
   }
 
@@ -31,8 +35,11 @@ export class MediaPlayerComponent implements OnInit, OnDestroy {
         console.log('Recibiendo track: ', res)
       }
     )
+    const observer2$: Subscription = this.trackService.getAllTracks().subscribe(
+      res => {this.mockCover = res.data[Math.floor(Math.random() * 8)];}  
+    )
 
-    this.listObservers$ = [observer1$]
+    this.listObservers$.push(observer1$,observer2$)
   }
 
   ngOnDestroy(): void {
